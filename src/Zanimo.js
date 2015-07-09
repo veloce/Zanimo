6334a7c839c9fbfead5deaed01e19c6890a79562
Vincent Velociter <vincent.velociter@gmail.com>
2015-07-09 23:58:09 +0200

    Use native Promises and remove Q

--- src/Zanimo.js
+++ src/Zanimo.js
@@ -3,9 +3,7 @@
 
 'use strict';
 
-var Q = require('q'),
-    QanimationFrame = require('qanimationframe'),
-    prefix = require('vendor-prefix'),
+var prefix = require('vendor-prefix'),
     normalizeTransformValue = require('../src/normalize-transform-value'),
     shorthand = require('../src/transition-shorthand-property'),
     transition = prefix('transition'),
@@ -16,14 +14,14 @@ var Q = require('q'),
             return el && el.nodeType;
         } catch(err) {
             return false;
-        };
+        }
     },
 
     addTransition = function (elt, attr, value, duration, easing) {
         var currentValue = elt.style[transition];
         attr = prefix.dash(attr);
         if (currentValue) {
-            elt.style[transition] = currentValue + ", " + shorthand(attr, duration, easing);
+            elt.style[transition] = currentValue + ', ' + shorthand(attr, duration, easing);
         }
         else {
             elt.style[transition] = shorthand(attr, duration, easing);
@@ -39,72 +37,74 @@ var Q = require('q'),
     },
 
     applycss = function (el, attr, value) {
-        return QanimationFrame(function(){
+        requestAnimationFrame(function(){
             el.style[prefix.dash(attr)] = value;
-            return el;
         });
+        return Promise(el);
     },
 
     css = function (el, attr, value) {
         if(el._zanimo && el._zanimo.hasOwnProperty(attr)) {
-            el._zanimo[attr].defer.reject(new Error(
-                "Zanimo transition with transform=" +
+            Promise.reject(new Error(
+                'Zanimo transition with transform=' +
                 el._zanimo[attr].value +
-                " stopped by transform=" + value
+                ' stopped by transform=' + value
             ));
-            el._zanimo[attr].cb();
         }
         return applycss(el, attr, value);
     },
 
     animate = function (el, attr, value, duration, easing) {
         var prefixed = prefix.dash(attr),
-            d = Q.defer(),
-            timeout,
-            cb = function (clear) {
+            timeout;
+
+        var promise = new Promise(function(resolve, reject) {
+
+            function cbTransitionend(evt) {
+                if(prefix(evt.propertyName) === prefix(prefixed)) {
+                    cb(true);
+                    resolve(el);
+                }
+            }
+
+            function cb(clear) {
                 if (timeout) { clearTimeout(timeout); timeout = null; }
                 removeTransition(el, attr);
                 el.removeEventListener(transitionend, cbTransitionend);
                 if (clear) { delete el._zanimo[attr]; }
-            },
-            cbTransitionend = function (evt) {
-                if(prefix(evt.propertyName) === prefix(prefixed)) {
-                    cb(true);
-                    d.resolve(el);
-                }
-            };
+            }
 
-        el.addEventListener(transitionend, cbTransitionend);
+            el.addEventListener(transitionend, cbTransitionend);
 
-        QanimationFrame(function () {
-            addTransition(el, attr, normalizeTransformValue(value), duration, easing);
-            timeout = setTimeout(function () {
-                var rawVal = el.style.getPropertyValue(prefixed),
+            requestAnimationFrame(function () {
+                addTransition(el, attr, normalizeTransformValue(value), duration, easing);
+                timeout = setTimeout(function () {
+                    var rawVal = el.style.getPropertyValue(prefixed),
                     domVal = normalizeTransformValue(rawVal),
                     givenVal = normalizeTransformValue(value);
 
-                cb(true);
-                if (domVal === givenVal) { d.resolve(el); }
-                else {
-                    d.reject( new Error("Zanimo transition: with "
-                        + attr + " = " + givenVal + ", DOM value=" + domVal
+                    cb(true);
+                    if (domVal === givenVal) { resolve(el); }
+                    else {
+                        reject(new Error('Zanimo transition: with '
+                            + attr + ' = ' + givenVal + ', DOM value=' + domVal
+                        ));
+                    }
+                }, duration + 20 );
+
+                el._zanimo = el._zanimo || { };
+                if(el._zanimo[attr]) {
+                    reject(new Error(
+                        'Zanimo transition with ' +
+                            attr + '=' + el._zanimo[attr].value +
+                            ' stopped by transition with ' + attr + '=' + value
                     ));
                 }
-            }, duration + 20 );
-
-            el._zanimo = el._zanimo || { };
-            if(el._zanimo[attr]) {
-                el._zanimo[attr].defer.reject(new Error(
-                    "Zanimo transition with " +
-                    attr + "=" + el._zanimo[attr].value +
-                    " stopped by transition with " + attr + "=" + value
-                ));
-                el._zanimo[attr].cb();
-            }
-            el._zanimo[attr] = {cb: cb, value: value, defer: d};
+                el._zanimo[attr] = {value: value};
+            });
         });
 
-        return d.promise;
+        return promise;
     };
 
 /**
@@ -121,29 +121,29 @@ var Zanimo = function (el, attr, value, duration, easing) {
     var args = arguments,
         arity = arguments.length;
     if (arity === 0 || arity === 2 || arity > 5) {
-        return Q.reject(new Error("Zanimo invalid arguments"));
+        return Promise.reject(new Error('Zanimo invalid arguments'));
     }
-    if (Q.isPromise(el)) {
+    if (el instanceof Promise) {
         return el.then(function (val) {
             return Zanimo.apply(this, [val].concat(Array.prototype.slice.call(args, 1)));
         });
     }
     if (!isDOM(el)) {
-        return Q.reject(new Error("Zanimo require an HTMLElement, or a promise of an HTMLElement"));
+        return Promise.reject(new Error('Zanimo require an HTMLElement, or a promise of an HTMLElement'));
     }
     if (arity === 1) {
-        return Q(el);
+        return Promise(el);
     }
     try {
         prefix.dash(attr);
     } catch(err) {
-        return Q.reject(new Error("Zanimo transition: " + attr + ' is not supported!'));
-    };
+        return Promise.reject(new Error('Zanimo transition: ' + attr + ' is not supported!'));
+    }
     if (arity === 3) {
         return css(el, attr, value);
     }
     if(window.isNaN(parseInt(duration, 10))) {
-        return Q.reject(new Error("Zanimo transition: duration must be an integer!"));
+        return Promise.reject(new Error('Zanimo transition: duration must be an integer!'));
     }
     return animate(el, attr, value, duration, easing);
 };
@@ -151,7 +151,7 @@ var Zanimo = function (el, attr, value, duration, easing) {
 /**
  * A function wrapping `Zanimo(el, ...)` as a `f(...)(el)` for easy chaining purpose.
  */
-Zanimo.f = function (attr, value, duration, easing) {
+Zanimo.f = function () {
     var args = Array.prototype.slice.call(arguments);
     return function (el) {
         return Zanimo.apply(this, [el].concat(args));
